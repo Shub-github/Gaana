f080232cdc7f4cc3f33bd69c8689f93c2611240a
shubham <swardshub@gmail.com>
2023-05-23 23:39:48 +0530

    push_code

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <!-- Component markup here -->
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello, Vue!",
+    };
+  },
+};
+</script>
